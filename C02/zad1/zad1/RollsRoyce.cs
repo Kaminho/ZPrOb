acb9556f60df113ecd8b593a8d7d066e425c59a3
Kaminho <kaminho16@gmail.com>
2017-03-10 16:46:31 +0100

    C02

---
+++ C02/zad1/zad1/RollsRoyce.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad1
+{
+    class RollsRoyce : ISamochod
+    {
+        public string WypiszMarke()
+        {
+            return "Rolls Royce";
+        }
+
+        public string WypiszSalon()
+        {
+            return "Salon Rolls Royce";
+        }
+    }
+}
